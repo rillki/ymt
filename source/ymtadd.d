04061247abacfd4cfe6f0116052d87615a6580e9
Ki Rill <rill.ki@yahoo.com>
2022-07-08 21:28:47 +0600

    implementing functionality for adding data to db

--- source/ymtadd.d
+++ source/ymtadd.d
@@ -1,6 +1,5 @@
 module ymtadd;
 
-import d2sqlite3: Database;
 import std.file: readText, exists;
 import std.path: buildPath;
 import std.stdio: writefln;
@@ -8,10 +7,10 @@ import std.format: format;
 
 import ymtcommon;
 
-void dbList(const string data) {
+void dbAddType(const string type) {
     // check if basedir exists
     if(!basedir.exists) {
-        writefln("#ymt: error! Initialize ymt first!");
+        writefln("#ymt add: error! Initialize ymt first!");
         return;
     }
 
@@ -20,7 +19,7 @@ void dbList(const string data) {
 
     // check if db exists
     if(!basedir.buildPath(dbname).exists) {
-        writefln("#ymt: error! %s does not exist, you need to initialize one!", dbname);
+        writefln("#ymt add: error! %s does not exist, you need to initialize one!", dbname);
         return;
     }
 
@@ -29,56 +28,77 @@ void dbList(const string data) {
 
     // prepare a querry
     immutable querry = `
-        SELECT * FROM %s
+        INSERT INTO ProductType (ProductType) VALUES ("%s")
     `;
 
-    // retrieve data
-    if(data == "types") {
-        auto results = db.execute(querry.format("ProductType"));
-        
-        // list data
-        writefln("\tID\tType");
-        foreach(row; results) {
-            auto id = row["ID"].as!uint;
-            auto type = row["ProductType"].as!string;
-            writefln("\t%s\t%s", id, type);
-        }
-    } else if(data == "names") {
-        auto results = db.execute(querry.format("ProductName"));
-        
-        // list data
-        writefln("\tID\tTypeID\tName");
-        foreach(row; results) {
-            auto id = row["ID"].as!uint;
-            auto typeID = row["ProductTypeID"].as!uint;
-            auto name = row["ProductName"].as!string;
-            writefln("\t%s\t%s\t%s", id, typeID, name);
-        }
-    } else if(data == "receipts") {
-        auto results = db.execute(querry.format("Receipt"));
-        
-        // list data
-        writefln("      Date\tTypeID\tNameID\tReceipt");
-        foreach(row; results) {
-            auto date = row["Date"].as!string;
-            auto typeID = row["ProductTypeID"].as!uint;
-            auto nameID = row["ProductNameID"].as!string;
-            auto receipt = row["ProductNameID"].as!long;
-            writefln("%s\t%s\t%s\t%s", date, typeID, nameID, receipt);
-        }
-    } else {
-        writefln("#ymt: %s is unknown! Choose either \'types\' or \'names\'.", data);
+    // add a new entry to database
+    try {
+        db.run(querry.format(type));
+    } catch(Exception e) {
+        writefln("\n#ymt add: %s\n", e.msg);
     }
 }
 
-void dbAddType(const string type) {
-    //...
-}
-
 void dbAddName(const string name, const uint typeID) {
-    //...
+    // check if basedir exists
+    if(!basedir.exists) {
+        writefln("#ymt add: error! Initialize ymt first!");
+        return;
+    }
+
+    // read config file to get db name
+    immutable dbname = basedir.buildPath(configFile).readText;
+
+    // check if db exists
+    if(!basedir.buildPath(dbname).exists) {
+        writefln("#ymt add: error! %s does not exist, you need to initialize one!", dbname);
+        return;
+    }
+
+    // open db
+    auto db = Database(basedir.buildPath(dbname));
+
+    // prepare a querry
+    immutable querry = `
+        INSERT INTO ProductName (ProductName, ProductTypeID) VALUES ("%s", %s)
+    `;
+
+    // add a new entry to database
+    try {
+        db.run(querry.format(name, typeID));
+    } catch(Exception e) {
+        writefln("\n#ymt add: %s\n", e.msg);
+    }
 }
 
 void dbAddReceipt(const float receipt, const uint nameID, const uint typeID) {
-    //...
+    // check if basedir exists
+    if(!basedir.exists) {
+        writefln("#ymt add: error! Initialize ymt first!");
+        return;
+    }
+
+    // read config file to get db name
+    immutable dbname = basedir.buildPath(configFile).readText;
+
+    // check if db exists
+    if(!basedir.buildPath(dbname).exists) {
+        writefln("#ymt add: error! %s does not exist, you need to initialize one!", dbname);
+        return;
+    }
+
+    // open db
+    auto db = Database(basedir.buildPath(dbname));
+
+    // prepare a querry
+    immutable querry = `
+        INSERT INTO Receipt (Date, ProductNameID, ProductTypeID, Receipt) VALUES (CURRENT_DATE, %s, %s, %s)
+    `;
+
+    // add a new entry to database
+    try {
+        db.run(querry.format(nameID, typeID, receipt));
+    } catch(Exception e) {
+        writefln("\n#ymt add: %s\n", e.msg);
+    }
 }

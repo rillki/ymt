dcbaa389d6cca7007404f6833f67de51a28cd7a1
Ki Rill <rill.ki@yahoo.com>
2022-07-08 21:28:21 +0600

    listing db data

---
+++ source/ymtlist.d
@@ -0,0 +1,70 @@
+module ymtlist;
+
+import std.file: readText, exists;
+import std.path: buildPath;
+import std.stdio: writefln;
+import std.format: format;
+import std.math: abs;
+
+import ymtcommon;
+
+void dbList(const string data) {
+    // check if basedir exists
+    if(!basedir.exists) {
+        writefln("#ymt list: error! Initialize ymt first!");
+        return;
+    }
+
+    // read config file to get db name
+    immutable dbname = basedir.buildPath(configFile).readText;
+
+    // check if db exists
+    if(!basedir.buildPath(dbname).exists) {
+        writefln("#ymt: error! %s does not exist, you need to initialize one!", dbname);
+        return;
+    }
+
+    // open db
+    auto db = Database(basedir.buildPath(dbname));
+
+    // prepare a querry
+    immutable querry = `
+        SELECT * FROM %s
+    `;
+
+    // retrieve data
+    if(data == "types") {
+        auto results = db.execute(querry.format("ProductType"));
+        
+        // list data
+        writefln("%5s   %s", "ID", "Type");
+        foreach(row; results) {
+            auto id = row["ID"].as!uint;
+            auto type = row["ProductType"].as!string;
+            writefln("%5s   %s", id, type);
+        }
+    } else if(data == "names") {
+        auto results = db.execute(querry.format("ProductName"));
+        
+        // list data
+        writefln("%5s   %6s   %s", "ID", "TypeID", "Name");
+        foreach(row; results) {
+            auto id = row["ID"].as!uint;
+            auto typeID = row["ProductTypeID"].as!uint;
+            auto name = row["ProductName"].as!string;
+            writefln("%5s   %6s   %s", id, typeID, name);
+        }
+    } else {
+        auto results = db.execute(querry.format("Receipt"));
+        
+        // list data
+        writefln("%10s   %6s   %6s   %s", "Date", "TypeID", "NameID", "Receipt");
+        foreach(row; results) {
+            auto date = row["Date"].as!string;
+            auto typeID = row["ProductTypeID"].as!uint;
+            auto nameID = row["ProductNameID"].as!string;
+            auto receipt = row["Receipt"].as!long;
+            writefln("%10s   %6s   %6s   %s", date, typeID, nameID, receipt);
+        }
+    }
+}

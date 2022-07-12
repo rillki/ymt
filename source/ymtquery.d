f6589cf81be9c0ea4410661078bcfbd46d445551
Ki Rill <rill.ki@yahoo.com>
2022-07-12 10:47:38 +0600

    direct db querying added

---
+++ source/ymtquery.d
@@ -0,0 +1,48 @@
+module ymtquery;
+
+import std.file: readText, exists;
+import std.path: buildPath;
+import std.stdio: writefln;
+import std.format: format;
+
+import ymtcommon;
+
+void dbQuery(const string query) {
+	// check if got valid query
+	if(query is null) {
+        writefln("#ymt query: no query provided!");
+        return;
+	}
+
+	// check if basedir exists
+    if(!basedir.exists) {
+        writefln("#ymt query: error! Initialize ymt first!");
+        return;
+    }
+
+    // read config file to get db name
+    immutable dbname = basedir.buildPath(configFile).readText;
+
+    // check if db exists
+    if(!basedir.buildPath(dbname).exists) {
+        writefln("#ymt: error! %s does not exist, you need to initialize one!", dbname);
+        return;
+    }
+
+    // open db
+    auto db = Database(basedir.buildPath(dbname));
+	
+	// execute query
+	writefln("");
+    try {
+		writefln("#ymt query: executed \"%s\"", query);
+
+        auto results = db.execute(query);
+		foreach(row; results) {
+			writefln("%s", row);
+		}
+    } catch(Exception e) {
+        writefln("#ymt query: %s", e.msg);
+    }
+	writefln("");
+}
